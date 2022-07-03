c5c7d67cf1da84a76f7a61552979a9acb3164135
Nguyen Gia Bao <nguyengiabbaob@gmail.com>
2022-07-03 19:55:35 +0700

    7/3

--- LongDaiNamContruction/ClientApp/src/App.js
+++ LongDaiNamContruction/ClientApp/src/App.js
@@ -8,6 +8,7 @@ var Counter_1 = require("./components/Counter");
 var FetchData_1 = require("./components/FetchData");
 require("antd/dist/antd.css");
 require("./custom.css");
+require("./Style/CustomStyle.scss");
 exports.default = (function () { return (React.createElement(Layout_1.default, null,
     React.createElement(react_router_1.Route, { exact: true, path: '/', component: Home_1.default }),
     React.createElement(react_router_1.Route, { path: '/counter', component: Counter_1.default }),
--- LongDaiNamContruction/ClientApp/src/components/Home/Home.tsx
+++ LongDaiNamContruction/ClientApp/src/components/Home/Home.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Typography } from 'antd';
 import '../../Style/CustomStyle.scss';
 import ActivityFields from "./ActivityFields";
+import SpecialConstruction from "./SpecialContruction";
 const Home =()=>{
 const {Title}= Typography;
     return (
-    <div>
+    <div style={{marginTop:'5em', zIndex:1000}}>
         <Title className="font-title">Lĩnh vực hoạt động</Title>
-        <p style={{position:'absolute', left: '25%', }}>Công trình của công ty thực hiện trên khắp cả nước luôn là những công trình nổi bật, phức tạp, đòi hỏi công nghệ xây dựng cập nhật nhất hiện nay.</p>
+        <p style={{textAlign:'center' }}>Công trình của công ty thực hiện trên khắp cả nước luôn là những công trình nổi bật, phức tạp, đòi hỏi công nghệ xây dựng cập nhật nhất hiện nay.</p>
         <ActivityFields/>
+        <SpecialConstruction/>
     </div> 
     )
 }
